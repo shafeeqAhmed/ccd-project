0ffcf90e78e2540dc081693764d0dd9aa17bac40
Zohaib Mazhar <110980291+zohaibmazhar075@users.noreply.github.com>
2023-02-09 17:45:34 +0500

    style login

--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="main-layout">
     <Navbar />
     <Nuxt />
     <Footer />
@@ -18,7 +18,7 @@ export default {
     ...mapGetters({
       authenticated: 'auth/authenticated',
       user: 'auth/user'
-    }),    
+    }),
  },
 
   mounted() {
