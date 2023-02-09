31e27e1b77b58c6fc48b2e8fbc091625b6db4bb7
Talha Fayyaz <42764771+talhafayyaz123@users.noreply.github.com>
2023-02-09 20:35:00 +0500

    menu bar fix

--- client/components/Navbar.vue
+++ client/components/Navbar.vue
@@ -2,37 +2,65 @@
   <nav class="header navbar navbar-expand-lg bg-dark navbar-dark">
     <div class="container">
       <!-- TODO. <a href="#" class="navbar-brand">Logo here</a> -->
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navmenu">
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navmenu"
+      >
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="sidebar-overlay has-dropdown is-hoverable collapse navbar-collapse" id="navmenu"
-        data-bs-toggle="collapse" data-bs-target="#navmenu"></div>
-      <div class="px-5 navbar-item has-dropdown is-hoverable collapse navbar-collapse" id="navmenu">
+      <div
+        class="sidebar-overlay has-dropdown is-hoverable collapse navbar-collapse"
+        id="navmenu"
+        data-bs-toggle="collapse"
+        data-bs-target="#navmenu"
+      ></div>
+      <div
+        class="px-5 navbar-item has-dropdown is-hoverable collapse navbar-collapse"
+        id="navmenu"
+      >
         <ul class="navbar-nav ms-auto">
           <li class="nav-item">
-            <NuxtLink to="/"  class="nav-link"> Home </NuxtLink>
+            <NuxtLink to="/" class="nav-link"> Home </NuxtLink>
           </li>
           <!-- <li class="nav-item">
             <a href="/news" class="nav-link">News</a>
           </li> -->
           <li class="nav-item">
-            <NuxtLink to="/pricing"  class="nav-link"> Pricing </NuxtLink>
+            <NuxtLink to="/pricing" class="nav-link"> Pricing </NuxtLink>
           </li>
           <li class="nav-item">
-            <a href="https://documenter.getpostman.com/view/11410028/TVCY5BQc" class="nav-link" target="_blank">API
-              Docs</a>
+            <a
+              href="https://documenter.getpostman.com/view/11410028/TVCY5BQc"
+              class="nav-link"
+              target="_blank"
+              >API Docs</a
+            >
           </li>
           <li class="nav-item">
-            <NuxtLink to="/candles"  class="nav-link"> Get Candles </NuxtLink>
+            <NuxtLink to="/candles" class="nav-link"> Get Candles </NuxtLink>
           </li>
-          <li v-if="user" class="nav-item dropdown">
-            <button class="btn btn-outline-primary dropdown-toggle" type="button" data-bs-toggle="dropdown"
-              aria-expanded="false">
+          <li
+            v-if="user && Object.keys(user).length > 0"
+            class="nav-item dropdown"
+          >
+            <button
+              class="btn btn-outline-primary dropdown-toggle"
+              type="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
               {{ user.username }}
             </button>
             <ul class="dropdown-menu">
               <li>
-                <NuxtLink to="/profile" class="dropdown-item text-decoration-none"> My Profile </NuxtLink>
+                <NuxtLink
+                  to="/profile"
+                  class="dropdown-item text-decoration-none"
+                >
+                  My Profile
+                </NuxtLink>
                 <button class="dropdown-item btn btn-primary" @click="logout">
                   Logout
                 </button>
@@ -74,7 +102,6 @@ export default {
       this.logOut();
     },
   },
-
 };
 </script>
 
