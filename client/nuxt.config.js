af4b42dd2e16a406afe839f8b62867077e6e89d7
Talha Fayyaz <42764771+talhafayyaz123@users.noreply.github.com>
2023-02-13 22:28:22 +0500

    middleware register

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -35,7 +35,9 @@ export default {
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [],
-
+  router: {
+    middleware: ["guest"],
+  },
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/axios
